68155e19e8965f9c2eb546580027c5f9be461fed
qwerrrqw <latiger95@gmail.com>
2024-07-03 16:10:10 +0900

    Initial commit

---
+++ Python_team_assign.py
@@ -0,0 +1,19 @@
+# ----- 코드 정의 ------
+class Member:
+    # TODO : 코드 구현이 필요합니다.
+
+    def display(self):
+        # TODO : 코드 구현이 필요합니다.
+        pass
+
+
+class Post:
+    # TODO : 코드 구현이 필요합니다.
+    pass
+
+
+# ----- 코드 실행 ------
+members = []
+posts = []
+
+# TODO : 코드 구현이 필요합니다.
