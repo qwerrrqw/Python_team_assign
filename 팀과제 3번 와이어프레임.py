3daecba7c258dc6c30d511db5c492cbc15984c67
jinyoung <jyk3311@naver.com>
2024-07-03 17:57:03 +0900

    와이어 프레임 수정

--- 팀과제 3번 와이어프레임.py
+++ 팀과제 3번 와이어프레임.py
@@ -28,21 +28,16 @@ class Member:
 #<담당: 김진영>
 class Post:
     # TODO : 코드 구현이 필요합니다.
-    # - 게시물 제목 (**`title`**)   (done)
-    # - 게시물 내용 (**`content`**)   (done)
-    # - 작성자 (**`author`**) : 회원의 `username`   (done)
+    # - 게시물 제목 (**`title`**)
+    # - 게시물 내용 (**`content`**)
+    # - 작성자 (**`author`**) : 회원의 `username`
 #author는 인터페이스 구현에서 username 으로 넣어주면 됨
 def __init__(self, title, content, author):
-<<<<<<< HEAD:팀과제 와이어프레임.py
-    self.title = title
-    self.content = content
-    self.author = author
-=======
     셀프.타이틀 = 타이틀
     셀프.컨텐트 = 컨텐트
-    셀프.어서 = 어서 
+    셀프.어서 = 어서
 #</담당: 김진영>
->>>>>>> c7c06ad189bc494e617dff9278986a2a9198dbea:팀과제 3번 와이어프레임.py
+
 
 # ----- 코드 실행 ------
 
@@ -64,7 +59,7 @@ for i in members:
 
 
 # - 각각의 회원이 게시글을 세개 이상 작성하는 코드, 만들어진 게시글 인스턴스들은 posts 빈리스트에 append를 써서 저장
->
+> ex) post.append(post('네임', '내용 아무거나', '유저네임'))
 >
 >
 
