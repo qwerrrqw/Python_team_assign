2af49b120fe6bad1399b65540b67844e38838680
jonghwa <on9236@naver.com>
2024-07-04 23:11:50 +0900

    추가

--- 3번 문제.py
+++ 3번 문제.py
@@ -20,11 +20,43 @@ class Post:
 
 # ----- 코드 실행 ------
 members = []
+
+m1 = Member('name', 'username', 'password')
+m2 = Member('name', 'username', 'password')
+m3 = Member('name', 'username', 'password')
+
+members.append(m1)
+members.append(m2)
+members.append(m3)
+
+print(members)
+
 posts = []
 
 # TODO : 코드 구현이 필요합니다.
 
+p1 = Post('supernova', '가능한 모든 가능성 무한 속의 너를 만나', 'aespa')
+p2 = Post('Armageddon', '악몽은 또 짙게 번져가', 'aespa')
+p3 = Post('Savage', '네 환각들이 점점 너를 구축할 이유가 돼', 'aespa')
+posts.append(p1)
+posts.append(p2)
+posts.append(p3)
 
+p4 = Post('Super Shy', '나 원래 말도 잘하고 그런데 왜 이런지', 'NewJeans')
+p5 = Post('Bubble Gum', '내 향기가 널 먼저 찾아가', 'NewJeans')
+p6 = Post('Supernatural', '너와 나 다시 한번 만나게', 'NewJeans')
+posts.append(p4)
+posts.append(p5)
+posts.append(p6)
+
+p7 = Post('Accendio', '주문 걸어 아센디오', 'IVE')
+p8 = Post('I AM', '어제랑 또 다른 짜릿한 나', 'IVE')
+p9 = Post('LOVE DIVE', '참을 수 없는 이끌림과 호기심', 'IVE')
+posts.append(p7)
+posts.append(p8)
+posts.append(p9)
+
+print(posts)
 
 
 #----------------------
