d342a57dbfe6de4272ece7e17a7e198bd6c25812
jinyoung <jyk3311@naver.com>
2024-07-05 16:41:11 +0900

    코드 최적화

--- 3번 문제.py
+++ 3번 문제.py
@@ -2,17 +2,19 @@ import hashlib
 # ----- 코드 정의 ------
 import hashlib
 
+# Member 클래스 구현
+
+
 class Member:
-    
+
     def __init__(self, name, username, password):
         self.name = name
         self.username = username
-        hash_password = hashlib.sha256(password.encode())
-        self.password = hash_password
-        self.password = self.hash_password(password)   
-    
+        self.password = self.hash_password(password)
+
     def hash_password(self, password):
-        return hashlib.md5(password.encode()).hexdigest()
+        return hashlib.sha256(password.encode()).hexdigest()
+
     def display(self):
         print(f'이름: {self.name}, 아이디: {self.username}')
 
@@ -28,23 +30,21 @@ class Post:
 # ----- 코드 실행 ------
 members = []
 
-m1 = Member('aespa', 'KARINA', 'password')
-m2 = Member('NewJeans', 'HANNI', 'password')
-m3 = Member('IVE', 'REI', 'password')
+# 맴버 인스턴스 생성
+m1 = Member('KARINA', 'aespa', 'password')
+m2 = Member('HANNI', 'NewJeans', 'password')
+m3 = Member('REI', 'IVE', 'password')
 
 members.append(m1)
 members.append(m2)
 members.append(m3)
 
-print(members)
+for member in members:
+    member.display()
 
 posts = []
 
-for member in members:
-    print(member)
-
-# TODO : 코드 구현이 필요합니다.
-
+# 포스터 작성
 p1 = Post('supernova', '가능한 모든 가능성 무한 속의 너를 만나', 'aespa')
 p2 = Post('Armageddon', '악몽은 또 짙게 번져가', 'aespa')
 p3 = Post('Savage', '네 환각들이 점점 너를 구축할 이유가 돼', 'aespa')
@@ -66,11 +66,8 @@ posts.append(p7)
 posts.append(p8)
 posts.append(p9)
 
-for member in members:
-    print(member.display())
 
-
-#----------------------
+# ----------------------
 
 def search_name(author, posts):
     titles = []
@@ -80,12 +77,15 @@ def search_name(author, posts):
     if titles:
         return titles
     return
+
+
 search_author = input("찾으실 글쓴이를 입력해주세요: ")
 search_titles = search_name(search_author, posts)
 if isinstance(search_titles, list):
     print(f"{search_author}가 작성한 게시글: {','.join(search_titles)}")
 else:
     print(f"{search_author}가 작성한 게시글이 없습니다.")
+
 
 def search_include(word, posts):
     titles = []
@@ -95,22 +95,14 @@ def search_include(word, posts):
     if titles:
         return titles
     return
+
+
 search_keyword = input("찾으실 검색어를 입력해주세요: ")
 search_titles = search_include(search_keyword, posts)
 if isinstance(search_titles, list):
     print(f"{search_keyword}가 들어간 게시글: {','.join(search_titles)}")
 else:
     print(f"{search_keyword}가 들어간 게시글이 없습니다.")
-# 입력받은 검색어가 들어간 포스트의 제목을 출력
-
-# **추가 도전 과제:**
-
-#  - input을 이용하여 Member 인스턴스 만드는것을 사용자가 터미널에서 할 수 있게 해주세요.    (done)
-
-#  - post도 터미널에서 생성할 수 있게 해주세요.     (done)
-# #  >> 어서 == 유저네임
-
-#  - (심화)비밀번호 해싱이 무엇인지 공부한 후 hashlib 라이브러리를 써서 회원 비밀번호를 해시화하여 저장하게 해주세요.
 
 new_name = input("이름을 입력하세요: ")
 new_user_name = input("아이디를 입력하세요: ")
@@ -120,33 +112,24 @@ members.append(m4)
 
 new_post = input("제목을 입력하세요: ")
 new_content = input("내용울 입력하세요: ")
+
+flag = False  # while문 탈출도구
 while True:
     new_author = input("작성자를 입력하세요: ")
     for member in members:
         if new_author == member.username:
             post10 = Post(new_post, new_content, new_author)
             posts.append(post10)
+            flag = True
             break
-        else:
-            print("입력하신 작성자의 아이디를 찾을수 없습니다. 다시 입력하세요!")
+    if flag == True:
+        break
+    print("입력하신 작성자의 아이디를 찾을수 없습니다. 다시 입력하세요!")
 
 print("회원 목록")
 for member in members:
-    print(member.display())
+    member.display()
 
 print("작성된 글 목록")
 for post in posts:
     print(f"제목: {post.title},작성자: {post.author}")
-'''
-담당: 나지수
-hashlib 라이브러리 참고 https://docs.python.org/ko/3/library/hashlib.html
-
-형식 >>>>>>>        import hashlib
-                    hash_object = hashlib.sha256(string.encode()) 
-
-MD5: 속도가 빠르지만 보안에 취약함. 현재는 보안 목적으로 사용하지 않음.
-SHA-1: MD5보다 보안성이 높지만, 현재는 보안 취약점 때문에 사용하지 않음.
-SHA-256: 높은 보안성과 적당한 속도로 널리 사용됨.
-SHA-512: SHA-256보다 더 높은 보안성을 제공하지만, 그만큼 속도가 느림.
-
-'''
